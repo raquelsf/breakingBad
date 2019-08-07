d808ea98f17b21e10578fb1e112f62ec8ae4622a
Raquel <raquelsfreitasmaia@gmail.com>
2019-08-07 01:12:59 -0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://www.breakingbadapi.com/api/',
+  suggestions: 'https://frontendtestesamba.free.beeceptor.com/',
 };
